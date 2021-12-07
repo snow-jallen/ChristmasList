679a06574ea99b2331b366e8541ea8f39282855d
Jonathan Allen <jallen@snow.edu>
2021-12-07 16:56:58 -0700

    fix connection string

--- Startup.cs
+++ Startup.cs
@@ -34,9 +34,7 @@ namespace ChristmasList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration["DATABASE_URL"]));
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
