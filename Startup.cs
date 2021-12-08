c73e8173adfdadddb2b1ed36363f9ec23e44351a
Jonathan Allen <jallen@snow.edu>
2021-12-07 17:01:11 -0700

    handle heroku connection string

--- Startup.cs
+++ Startup.cs
@@ -34,13 +34,31 @@ namespace ChristmasList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration["DATABASE_URL"]));
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(convertUrlConnectionString(Configuration["DATABASE_URL"])));
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
             services.AddScoped<CatalogService>();
             services.AddScoped<ICatalogDataService, DefaultCatalogDataService>();
+        }
+
+        private static string convertUrlConnectionString(string url)
+        {
+            if (url is null)
+                throw new ArgumentNullException("It appears you're missing the DATABASE_URL configuration value...");
+            if (!url.Contains("//"))
+                return url;
+            var uri = new Uri(url);
+            var host = uri.Host;
+            var port = uri.Port;
+            var database = uri.Segments.Last();
+            var parts = uri.AbsoluteUri.Split(':', '/', '@');
+            var user = parts[3];
+            var password = parts[4];
+
+            return $"host={host}; port={port}; database={database}; username={user}; password={password}; SSL Mode=Prefer; Trust Server Certificate=true";
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
